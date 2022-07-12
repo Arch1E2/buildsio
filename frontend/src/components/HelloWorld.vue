d74948c11d0b74a3d59917033bd3f146282d5142
Sergey Bykov <derter@kitendo.ru>
2022-07-12 16:38:36 +0400

    frontend init

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <el-card>HelloWRLD</el-card>
+  </div>
+</template>
+
+<script>
+// import axios from "axios"
+// import {ElButton, ElDatePicker, ElCard} from "element-plus"
+</script>
