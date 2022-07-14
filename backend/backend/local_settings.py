a2e8d7c664c91a7d25a31dedfca0045b2c985496
arch1 <arch1e2.work@gmail.com>
2022-07-14 19:54:16 +0400

    dockerize and fixes

---
+++ backend/backend/local_settings.py
@@ -0,0 +1,13 @@
+import os 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'builds_db',
+        'PORT': '5432',
+    }
+}
+
--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Run Back"
 sleep 3
@@ -7,10 +7,7 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py get_valutes
 python manage.py get_valute_rates
-ls
 crontab ../etc/cron.d/get_valute_rates
 touch ../var/log/cron.log
-cron && tail -f ../var/log/cron.log
-echo "cron run"
-python manage.py runserver 8000
 
+python manage.py runserver 0:8000
